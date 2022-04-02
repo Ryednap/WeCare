bd35ade13b2f95bc3464502f959a24eadbb14e76
Ryednap <ujjwalpandey408@gmail.com>
2022-04-02 21:41:11 +0530

    Intialized Microservice MedicineDispatcher

---
+++ backendServices/MedicineDispatcher/src/main/java/com/iot/backend/services/medicinedispatcher/MedicineDispatcherApplication.java
@@ -0,0 +1,11 @@
+package com.iot.backend.services.medicinedispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineDispatcherApplication {
+    public static void main (String [] args) throws Exception {
+        SpringApplication.run(MedicineDispatcherApplication.class, args);
+    }
+}
