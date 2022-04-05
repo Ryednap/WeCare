edd51562f11c0a5b25bcee2760e4cca6dd30e1d3
Ryednap <ujjwalpandey408@gmail.com>
2022-04-05 08:10:56 +0530

    make it better

--- MicroController/NodeMCU1.0/src/main.cpp
+++ MicroController/NodeMCU1.0/src/main.cpp
@@ -1,40 +1,88 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <ArduinoJson.h>
 
+// Server configuration
+const String server = "http://localhost:8080/";
+const String path = "/api/v1/medicine";
 
-String server = "http://dummy.restapiexample.com/api/v1/employees";
-String SSID = "Redmi Note 10S";
-String password = "ujhas8balls";
+[[maybe_unused]] const IPAddress ipAddress(10,42,0,1);
+[[maybe_unused]] const int httpPort = 3000;
 
+// Wifi configuration
+const String SSID = "h2smedia";
+const String password = "helloWorld";
+
+// Machine Configuration
 HTTPClient http;
+WiFiClient client;
+
+
+void printBanner() {
+    Serial.println("  _  _            _         __  __    ___   _   _     __  __               _\n"
+                   " | \\| |  ___   __| |  ___  |  \\/  |  / __| | | | |   |  \\/  |  __ _   ___ | |_   ___   _ _\n"
+                   " | .` | / _ \\ / _` | / -_) | |\\/| | | (__  | |_| |   | |\\/| | / _` | (_-< |  _| / -_) | '_|\n"
+                   " |_|\\_| \\___/ \\__,_| \\___| |_|  |_|  \\___|  \\___/    |_|  |_| \\__,_| /__/  \\__| \\___| |_|\n"
+                   );
+}
+
+int attempt = 0;
 
 void setup() {
     Serial.begin(115200);
-    delay(10);
+    delay(500);
     Serial.println("\n");
+    printBanner();
+    Serial.println("Connecting to .. ");
+    Serial.println(SSID);
 
-    WiFi.begin(SSID.c_str(), password.c_str());
-    Serial.print("Connecting to..");
-
-    int i = 0;
+    WiFi.begin(SSID, password);
     while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.print(++i); Serial.print(' ');
+        delay(500);
+        Serial.print(".");
     }
-    Serial.println('\n');
-    Serial.println("Connection established!");
-    Serial.print("IP address:\t");
+
+    Serial.println("");
+    Serial.println("Wifi Connected");
+    Serial.println("IP Address..");
     Serial.println(WiFi.localIP());
 }
 
 void loop() {
-    WiFiClient client;
-    http.begin(client, server);
+    delay(5000);
 
+    // use WiFiClient to create TCP connection to create a transport layer
+    // to transport the HTTP content
+
+    if (!client.connect(ipAddress, httpPort)) {
+        Serial.printf("Attempt %d: Connection Failed....\n", attempt);
+        ++attempt;
+        return;
+    } else {
+        Serial.println("Connection Established to the server....\n");
+        attempt = 0;
+    }
+
+    http.begin(client, server + path);
+    delay(10);
     int responseCode = http.GET();
+    delay(10);
+    Serial.printf("Response Code received From server... : %d\n", responseCode);
     String payload = http.getString();
-    Serial.println("payload = " + payload);
-    delay(2000);
+
+
+    // JSON serialization
+    ArduinoJson6193_F1::StaticJsonDocument<200> doc;
+    ArduinoJson6193_F1::DeserializationError deserializationError = ArduinoJson6193_F1::deserializeJson(doc, payload);
+    delay(100);
+
+    if (deserializationError) {
+        Serial.print(F("deserializeJson() Failed: "));
+        Serial.println(deserializationError.f_str());
+        return;
+    }
+
+    Serial.println("\n\n Message Payload");
+    ArduinoJson6193_F1::serializeJsonPretty(doc, Serial);
 }
